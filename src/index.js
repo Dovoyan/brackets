c34c0b1f81253128712372b22e62fad9a96379bb
Dovoyan <yankavalenkau@gmail.com>
2021-02-18 19:57:31 +0300

    iT WORK

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = [];
+  let i = 0;
+  let x = str.split('')
+  bracketsConfig.forEach(element => {
+
+    a[i] = element.join('');
+    i++;
+  });
+
+  x.forEach(element => {
+    a.forEach(element1 => {
+      str = str.replace(element1, '')
+    });
+
+  });
+
+
+
+  if (str == '') {
+    return true;
+
+  } else {
+    return false;
+  }
 }
+
